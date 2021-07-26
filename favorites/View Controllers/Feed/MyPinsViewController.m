c02029c1a737349f739e7ed93375acf84bb25558
Ava Crnkovic-Rubsamen <avacrnkovic@fb.com>
2021-07-26 16:39:07 -0400

    make search bar functional on my pins view controller

--- favorites/View Controllers/Feed/MyPinsViewController.m
+++ favorites/View Controllers/Feed/MyPinsViewController.m
@@ -10,9 +10,11 @@
 #import <Parse/Parse.h>
 #import "Pin.h"
 
-@interface MyPinsViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface MyPinsViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSArray *pins;
+@property (strong, nonatomic) NSMutableArray *filteredPins;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @end
 
@@ -22,6 +24,7 @@
     [super viewDidLoad];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    self.searchBar.delegate = self;
     [self getPins];
 }
 
@@ -38,6 +41,7 @@
             if (pins != nil) {
                 strongSelf.pins = pins;
                 NSLog(@"got pins");
+                strongSelf.filteredPins = [NSMutableArray arrayWithArray:strongSelf.pins];
                 [strongSelf.tableView reloadData];
             } else {
                 NSLog(@"%@", error.localizedDescription);
@@ -47,21 +51,40 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (self.pins) {
-        NSLog(@"%lu", (unsigned long)[self.pins count]);
-        return [self.pins count];
-    }
+    if (self.filteredPins) return [self.filteredPins count];
     else return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     MyPinCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyPinCell" forIndexPath:indexPath];
-    Pin *pin = self.pins[indexPath.row];
-    if (self.pins) {
+    if (self.filteredPins) {
+        Pin *pin = self.filteredPins[indexPath.row];
         cell.titleLabel.text = pin.title;
         cell.pin = pin;
     }
     return cell;
+}
+# pragma mark - Search Bar Functions
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    if (searchText.length != 0) {
+        NSArray *filteredPins = [self.pins filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"(title contains[c] %@)", searchText]];
+        self.filteredPins = [NSMutableArray arrayWithArray:filteredPins];
+    }
+    else {
+        self.filteredPins = [NSMutableArray arrayWithArray:self.pins];
+    }
+    [self.tableView reloadData];
+}
+
+-(void) searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    self.searchBar.showsCancelButton = YES;
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    self.searchBar.showsCancelButton = NO;
+    self.searchBar.text = @"";
+    [self.searchBar resignFirstResponder];
 }
 #pragma mark - Navigation
 
